6e052e66c6fab84ece08c1dfac22dc3109d2925c
Artsiom Shamsutdzinau <shamsartem@gmail.com>
2021-04-10 11:46:49 +0300

    Change adapter

--- svelte.config.cjs
+++ svelte.config.cjs
@@ -1,6 +1,6 @@
-const sveltePreprocess = require('svelte-preprocess');
-const node = require('@sveltejs/adapter-node');
-const pkg = require('./package.json');
+const sveltePreprocess = require('svelte-preprocess')
+const static = require('@sveltejs/adapter-static')
+const pkg = require('./package.json')
 
 /** @type {import('@sveltejs/kit').Config} */
 module.exports = {
@@ -11,15 +11,15 @@ module.exports = {
 		// By default, `npm run build` will create a standard Node app.
 		// You can create optimized builds for different platforms by
 		// specifying a different adapter
-		adapter: node(),
+		adapter: static(),
 
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
 
 		vite: {
 			ssr: {
-				noExternal: Object.keys(pkg.dependencies || {})
-			}
-		}
-	}
-};
+				noExternal: Object.keys(pkg.dependencies || {}),
+			},
+		},
+	},
+}
